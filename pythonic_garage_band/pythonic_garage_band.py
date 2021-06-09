b514951e57e73d352e44f3a5dd14514cbc33ecc4
abdullah-alsawalmeh <abdullahalsawalmeh@gmail.com>
2021-06-10 02:19:25 +0300

    Start Coding

---
+++ pythonic_garage_band/pythonic_garage_band.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+class Musician(ABC):
+    pass
+
+class Band:
+    pass
+
+class Guitarist:
+    pass
+
+class Bassist:
+    pass
+
+class Drummer:
+    pass
+
