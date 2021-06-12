537dabf819cd4dc0b2262d6ce94675a2344b4e21
abdullah-alsawalmeh <abdullahalsawalmeh@gmail.com>
2021-06-13 01:02:44 +0300

    Finish lab 04

--- pythonic_garage_band/pythonic_garage_band.py
+++ pythonic_garage_band/pythonic_garage_band.py
@@ -1,17 +1,78 @@
 from abc import ABC, abstractmethod
 
+class Band():
+    instances= []
+    def __init__(self,name,members):
+        self.name = name
+        self.members = members
+        self.instances.append(self)
+    def play_solos(self):
+        appended_solos=[]
+        for i in self.members:
+            appended_solos.append(i.play_solo())
+        return appended_solos
+    @classmethod
+    def to_list(cls):
+        return cls.instances
+    def __str__(self) -> str:
+        return "Great Band"
+    def __repr__(self) -> str:
+        return "Nothong can beat us"
+        
 class Musician(ABC):
-    pass
+    def __init__(self,name):
+        self.name = name
+    @abstractmethod
+    def get_instrument(self):
+        pass
+    @abstractmethod
+    def __str__(self) -> str:
+        pass
+    @abstractmethod
+    def __repr__(self) -> str:
+        pass
 
-class Band:
-    pass
+class Guitarist(Musician):
+    instrunment = "guitar"
+    def __str__(self) -> str:
+        return f"My name is {self.name} and I play {self.instrunment}"
+    def __repr__(self) -> str:
+        return f"Guitarist instance. Name = {self.name}"
+    def play_solo(self):
+        return "face melting guitar solo"
+    def get_instrument(self):
+        return self.instrunment
 
-class Guitarist:
-    pass
+class Drummer(Musician):
+    instrunment = "drums"
+    def __str__(self) -> str:
+        return f"My name is {self.name} and I play {self.instrunment}"
+    def __repr__(self) -> str:
+        return f"Drummer instance. Name = {self.name}"
+    def play_solo(self):
+        return "rattle boom crash"
+    def get_instrument(self):
+        return self.instrunment
 
-class Bassist:
-    pass
+class Bassist(Musician):
+    instrunment = "bass"
+    def __str__(self) -> str:
+        return f"My name is {self.name} and I play {self.instrunment}"
+    def __repr__(self) -> str:
+        return f"Bassist instance. Name = {self.name}"
+    def play_solo(self):
+        return "bom bom buh bom"
+    def get_instrument(self):
+        return self.instrunment
 
-class Drummer:
-    pass
-
+if __name__ == "__main__":
+    members = [
+        Guitarist("Kurt Cobain"),
+        Bassist("Krist Novoselic"),
+        Drummer("Dave Grohl"),
+    ]
+    some_band = Band("Nirvana", members)
+    joan = Guitarist("Joan Jett")
+    print(str(joan))
+    the_nobodies = Band("The Nobodies", [])
+    print(Band.instances[0])
